b403effc584a3d9aab789f409c2e4421e605e9f2
Nolan Gilley <nkgilley@gmail.com>
2017-02-09 08:34:21 -0500

    remove article data from popup

--- popup.js
+++ popup.js
@@ -1,37 +1,3 @@
-chrome.runtime.onMessage.addListener(function(request, sender) {
-  if (request.action == "getSource") {
-    //alert(request.source.length);
-    message.innerText = request.source;
-  }
-});
-
-function onWindowLoad() {
-
-  var message = document.querySelector('#message');
-
-  chrome.tabs.executeScript(null, {
-    file: "firebase.js"
-  }, function() {
-    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-    if (chrome.runtime.lastError) {
-      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
-    }
-  });
-  chrome.tabs.executeScript(null, {
-    file: "run.js"
-  }, function() {
-    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-    if (chrome.runtime.lastError) {
-      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
-    }
-  });
-
-}
-
-window.onload = onWindowLoad;
-
-
-
 var sourceDataCount = {};
 
 var config = {
