61de070a7b2019eea58f0bcecc58600aa6d76c34
Nolan Gilley <nkgilley@gmail.com>
2017-02-10 12:00:17 -0500

    pie colors, donut, arc padding

--- popup.js
+++ popup.js
@@ -42,12 +42,11 @@ chrome.identity.getProfileUserInfo(function(userInfo) {
   height = 400,
   radius = Math.min(width, height) / 2;
 
-  var color = d3.scaleOrdinal()
-      .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+  var color = d3.scaleOrdinal(d3.schemeCategory10);
 
   var arc = d3.arc()
       .outerRadius(radius - 10)
-      .innerRadius(0);
+      .innerRadius(radius - 100);
 
   var labelArc = d3.arc()
       .outerRadius(radius - 40)
@@ -55,6 +54,7 @@ chrome.identity.getProfileUserInfo(function(userInfo) {
 
   var pie = d3.pie()
       .sort(null)
+      .padAngle(.02)
       .value(function(d) { return d.count; });
 
   var svg = d3.select("body").append("svg")
@@ -77,7 +77,10 @@ chrome.identity.getProfileUserInfo(function(userInfo) {
       .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
       .attr("dy", ".35em")
       .text(function(d) { return d.data.source; });
-
+  g.append("text")
+      .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+      .attr("dy", "1.35em")
+      .text(function(d) { return d.data.count; });
   });
 });
 
