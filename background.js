aaf152ff635f20b833951625bf9d906d430f44db
Daniel McNally <mcnallydp@gmail.com>
2017-03-25 19:19:21 -0400

    Authentication bug fix
    Fixing a bug with how credentials are passed from background to content
    scripts
    Minor code refactoring and cleanup

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
-var user_id;
 var user_email;
 var read_count = 0;
 var database;
+var authToken;
 chrome.identity.getProfileUserInfo(function(userInfo) {
     user_id = userInfo.id;
     user_email = userInfo.email;
@@ -16,57 +16,14 @@ chrome.identity.getAuthToken({
         return;
     }
 	
-	var config = {
-		apiKey: "AIzaSyBb2F9FgRd69-B_tPgShM2CWF9lp5zJ9DI",
-		authDomain: "feedback-f33cf.firebaseapp.com",
-		databaseURL: "https://feedback-f33cf.firebaseio.com",
-		storageBucket: "feedback-f33cf.appspot.com",
-		messagingSenderId: "17295082044"
-	};
-	firebase.initializeApp(config);
-	
-	firebase.auth().signInWithCredential(firebase.auth.GoogleAuthProvider.credential(null, token)).then(function(user) {
-		database = firebase.database()
-	}).catch(function(error) {
-	  if(error) {
-		  
-	  }
-	  
- });
+	authToken = token;
 });
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.msg == "getUser") {
-        sendResponse({ 'email': user_email, 'id': user_id, 'database': database });
-        return true;
-    }
-});
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.msg == "increaseReadCount") {
+    if (request.msg === "getUser") {
+        sendResponse({ 'email': user_email, 'id': user_id, 'authToken': authToken });
+    } else if (request.msg == "increaseReadCount") {
         read_count += 1
         chrome.browserAction.setBadgeBackgroundColor({ color: [0, 0, 0, 1] });
         chrome.browserAction.setBadgeText({ text: read_count.toString() });
-        // var config = {
-        //     apiKey: "AIzaSyBb2F9FgRd69-B_tPgShM2CWF9lp5zJ9DI",
-        //     authDomain: "feedback-f33cf.firebaseapp.com",
-        //     databaseURL: "https://feedback-f33cf.firebaseio.com",
-        //     storageBucket: "feedback-f33cf.appspot.com",
-        //     messagingSenderId: "17295082044"
-        // };
-
-        // firebase.initializeApp(config);
-
-        // chrome.identity.getProfileUserInfo(function(userInfo) {
-        //     sourceDataCount = sourceDataCount;
-        //     user_id = userInfo.id;
-        //     user_email = userInfo.email;
-
-        //     return firebase.database().ref('/users/' + user_id).once('value').then(function(snapshot) {
-
-        //         user = snapshot.val();
-        //         articles = user && user.articles;
-        //         chrome.browserAction.setBadgeBackgroundColor({ color: [50, 0, 0, 50] });
-        //         chrome.browserAction.setBadgeText({ text: Object.keys(articles).length.toString() });
-        //     });
-        // });
     }
 });
