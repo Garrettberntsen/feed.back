3b4f8e80348788f5b6cd4096860286fb2381c1e4
mvaden <mvaden08@gmail.com>
2017-04-14 23:49:28 -0400

    Add and initialize Firebase.

---
+++ dashboard/config.firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBb2F9FgRd69-B_tPgShM2CWF9lp5zJ9DI",
+    authDomain: "feedback-f33cf.firebaseapp.com",
+    databaseURL: "https://feedback-f33cf.firebaseio.com",
+    projectId: "feedback-f33cf",
+    storageBucket: "feedback-f33cf.appspot.com",
+    messagingSenderId: "17295082044"
+  };
+  firebase.initializeApp(config);
