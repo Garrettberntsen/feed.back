c3eabb8460935eab4885e3cd6fc77fbdb21ebe12
Daniel McNally <mcnallydp@gmail.com>
2017-04-15 01:08:43 -0400

    Ratings submission
    Enables users to submit ratings for political lean and quality from the
    pop up.

--- popup.js
+++ popup.js
@@ -1,3 +1,16 @@
+// Function to create hashes for article keys
+String.prototype.hashCode = function() {
+    var hash = 0,
+        i, chr, len;
+    if (this.length === 0) return hash;
+    for (i = 0, len = this.length; i < len; i++) {
+        chr = this.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+};
+
 var sourceDataCount = {};
 
 var bg = chrome.extension.getBackgroundPage();
@@ -42,7 +55,7 @@ bg.database.ref('/users/' + bg.user_id).once('value').then(function(snapshot) {
 	  .padAngle(0.02)
 	  .value(function(d) { return d.count; });
 
-	var svg = d3.select("body").append("svg")
+	var svg = d3.select("#donut").append("svg")
 	  .attr("width", width)
 	  .attr("height", height)
 	.append("g")
@@ -66,7 +79,33 @@ bg.database.ref('/users/' + bg.user_id).once('value').then(function(snapshot) {
 	  .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
 	  .attr("dy", "1.35em")
 	  .text(function(d) { return d.data.count; });
-	});
+});
 
+$(function() {
+  $('#leanRating').barrating({
+	theme: 'bars-movie'
+  });
+  $('#starRating').barrating({
+	theme: 'fontawesome-stars'
+  });
+});
 
+$(document).ready(function(){
+    $("form").submit(function(){
+        chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+			var url = tabs[0].url.replace(/.*?:\/\/(www\.)?/, '').replace(/(\.html?).*/, '$1');
+			var article_key = url.hashCode();
+			bg.database.ref('/users/' + bg.user_id  + '/articles/' + article_key).once('value').then(function(snapshot) {
+				if(snapshot.exists()) {
+					bg.database.ref('users/' + bg.user_id + '/articles/' + article_key + '/stars').set($('#starRating').val());
+					bg.database.ref('users/' + bg.user_id + '/articles/' + article_key + '/lean').set($('#leanRating').val());
+					alert("Ratings submitted!");
+				} else {
+					alert("Not a valid article");
+				}
+			});
+		});
+		return false;
+    });
+});
 
