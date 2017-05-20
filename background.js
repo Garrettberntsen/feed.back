eb227377a3fac7973739950f6e613af219a66c8f
Damien Marble <damienmarble@gmail.com>
2017-05-20 12:43:27 -0700

    Moved last visit ur into articles.

--- background.js
+++ background.js
@@ -4,8 +4,6 @@
  */
 var read_count = 0;
 
-var last_visited_url;
-
 analytics.then(function () {
     ga("send", {
         hitType: "event",
@@ -37,124 +35,6 @@ function setReadCount(new_count) {
     }
 }
 
-function calculateAverageRatingForArticle(url) {
-    "use strict";
-    url = reduceUrl(url);
-    var article_id;
-    return current_articles[url].then(function (article) {
-        article_id = article.article_data.url.hashCode();
-        return getArticle(article_id);
-    }).catch(function (e) {
-        triggerGoogleAnalyticsEvent({
-            exDescription: JSON.stringify(e),
-            exFatal: true
-        })
-    })
-        .then(function (article) {
-            if (article.readers) {
-                return Promise.all(Object.keys(article.readers).map(function (reader_id) {
-                    return getUser(reader_id);
-                })).then(function (readers) {
-                    return readers.map(function (reader) {
-                        return reader.articles[article_id].stars;
-                    })
-                }).then(function (ratings) {
-                    var rating_count = 0;
-                    var rating_sum = ratings.reduce(function (total, next) {
-                        var next_rating = Number.parseInt(next);
-                        if (!isNaN(next_rating)) {
-                            total += next_rating;
-                            rating_count++;
-                        }
-                        return total;
-                    }, 0);
-                    if (rating_count) {
-                        return Promise.resolve(rating_sum / rating_count);
-                    } else {
-                        return Promise.resolve(0);
-                    }
-                });
-            } else {
-                return Promise.resolve(0);
-            }
-        }).catch(function (e) {
-            triggerGoogleAnalyticsEvent({
-                exDescription: JSON.stringify(e),
-                exFatal: true
-            })
-        });
-}
-
-function calculateAverageLeanForArticle(url) {
-    "use strict";
-    url = reduceUrl(url);
-    var article_id;
-    return current_articles[url].then(function (article) {
-        article_id = article.article_data.url.hashCode();
-        return getArticle(article_id);
-    }).catch(function (e) {
-        triggerGoogleAnalyticsEvent({
-            exDescription: JSON.stringify(e),
-            exFatal: true
-        })
-    })
-        .then(function (article) {
-            if (article.readers) {
-                return Promise.all(Object.keys(article.readers).map(function (reader_id) {
-                    return getUser(reader_id);
-                })).then(function (readers) {
-                    return readers.map(function (reader) {
-                        return reader.articles[article_id].lean;
-                    })
-                }).then(function (ratings) {
-                    var rating_count = 0;
-                    var rating_sum = ratings.reduce(function (total, next) {
-                        var next_rating = Number.parseInt(next);
-                        if (!isNaN(next_rating)) {
-                            total += next_rating;
-                            rating_count++;
-                        }
-                        return total;
-                    }, 0);
-                    if (rating_count) {
-                        return Promise.resolve(rating_sum / rating_count);
-                    } else {
-                        return Promise.resolve(0);
-                    }
-                });
-            } else {
-                return Promise.resolve(0);
-            }
-        }).catch(function (e) {
-            triggerGoogleAnalyticsEvent({
-                exDescription: JSON.stringify(e),
-                exFatal: true
-            })
-        });
-};
-
-function updateCurrentArticle(sender, message) {
-    "use strict";
-    if (!sender.tab) {
-        if (last_visited_url) {
-            current_articles[reduceUrl(last_visited_url)] = Promise.resolve(message);
-            current_user.then(function (user) {
-                writeArticleData(message, user);
-            });
-        }
-        ;
-    } else {
-        if (!tab_urls[sender.tab.id]) {
-            tab_urls[sender.tab.id] = [];
-        }
-        tab_urls[sender.tab.id].push(reduceUrl(sender.tab.url));
-        current_articles[reduceUrl(sender.tab.url)] = Promise.resolve(message);
-        current_user.then(function (user) {
-            writeArticleData(message, user);
-        });
-    }
-}
-
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     switch (request.type) {
         case "incrementReadCount":
@@ -169,49 +49,6 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         }
     }
 });
-
-function writeArticleData(article, user) {
-    if (!article || !article.article_data || !article.user_metadata.dateRead || !article.article_data.url) {
-        if (!article) {
-            console.log("writeArticleData null article passed.");
-        } else {
-            if (!article.article_data) {
-                console.log("writeArticleData was called with no data.");
-            } else {
-                if (!article_data.url) {
-                    console.log("writeArticleData called without defined url");
-                }
-
-                if (!article.user_metadata.dateRead) {
-                    console.log("writeArticleData date read not set");
-                }
-            }
-        }
-
-        return;
-    }
-    var article_data = article.article_data;
-
-    var article_key = article_data.url.hashCode();
-
-    _firebase.then(function (firebase) {
-        //Check if the article has already been scraped or the new record is not a partial record.
-        firebase.database().ref('articles/' + article_key).once("value").then(function (articleSnapshot) {
-            var existing_article;
-            if (articleSnapshot.exists()) {
-                existing_article = articleSnapshot.val();
-            }
-            if (!existing_article || !article_data.partialRecord) {
-                firebase.database().ref('articles/' + article_key).set(article_data);
-                if (article.user_metadata) {
-                    firebase.database().ref('users/' + user.id + '/articles/' + article_key).set(article.user_metadata);
-                }
-            }
-        });
-        firebase.database().ref('users/' + user.id + '/email').set(user.email);
-        console.log("feed.back data written to firebase!");
-    });
-}
 
 /*
  * Replace leading www. and http: and https:, trailing # fragment and query parameters.
