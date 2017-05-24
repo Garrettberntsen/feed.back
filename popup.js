49292562d8ee7caaf6f735d31b158b58b84173af
Leo Generali <leogen17@gmail.com>
2017-05-23 22:02:11 -0400

    Add a random wait time to track request to make it feel like it's doing something

--- popup.js
+++ popup.js
@@ -535,26 +535,21 @@ function addTrackThisQuestion() {
     var btn = document.createElement("BUTTON");
     var btnText = document.createTextNode("Yes - this is news!");
     btn.addEventListener("click", function(){
+        //A little silly - but this creates a random number between one and two seconds long 
+        //to fake doing something in the background.
+        var randomWaitTime = Math.floor(Math.random() * (2000 - 1000)) + 1000;
         btn.className += " is-loading";
         setTimeout(function(){
             btn.className = "dashboard-button button is-primary is-large is-news has-text-centered";
             var btnTextSent = "Got it! Thanks! &#128077";
             btn.innerHTML = btnTextSent;
-            console.log("sent!")
-        }, 1500)
-
-
-
+        }, randomWaitTime);
     })
     btn.appendChild(btnText);
-
     btn.className = "dashboard-button button is-primary is-large is-news has-text-centered";
-
-    console.log("trackElem");
-    console.log(trackElem);
-    console.log(question);
-    console.log(btn);
 
     trackElem.appendChild(question);
     trackElem.appendChild(btn);
+
+    // function acknowledgeSent
 }
