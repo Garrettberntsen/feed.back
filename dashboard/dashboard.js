39568e2e7e6f68f6b1a4a5f0862a9eda48f6051c
Leo Generali <leogen17@gmail.com>
2017-06-22 22:47:53 -0400

    Make goals page appear

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -6,8 +6,6 @@ chrome.runtime.sendMessage({type: "forcePersist"}, function(){
 chrome.runtime.sendMessage({type: "getUser"}, function (user) {
 	chrome.extension.getBackgroundPage()._firebase.then(function (firebase) {
 		firebase.database().ref("users/" + user.id).once("value").then(function (userSnapshot) {
-			
-
 			var articles = userSnapshot.val().articles;
 			var article_ids = [];
 			var article_definitions = [];
@@ -46,6 +44,8 @@ chrome.runtime.sendMessage({type: "getUser"}, function (user) {
 
 					var donutDataset = createDonutChartDataset(articleCountInTimespan);
 					var barchartDataset = createBarChartDataset(articlesInTimespan);
+
+					console.log(createBarChart);
 
 					createBarChart(barchartDataset);
 					createDonutChart(donutDataset);
@@ -159,131 +159,6 @@ chrome.runtime.sendMessage({type: "getUser"}, function (user) {
 							return Object.keys( articleTimespanTemplate ).sort();
 						}
 					}
-
-					// function createBarChart(dataset) {
-					// 	var margin = {
-					// 		top: 10,
-					// 		right: 0,
-					// 		bottom: 25,
-					// 		left: 30
-					// 	};
-
-					// 	var width = 960 - margin.left - margin.right,
-					// 		height = 288 - margin.top - margin.bottom;
-
-					// 	var svg = d3.select("div.bar-chart")
-					// 		.append("svg")
-					// 		.attr("class","chart chart--font")
-					// 		.attr("width", width + margin.left + margin.right)
-					// 		.attr("height", height + margin.top + margin.bottom)
-					// 		.append("g")
-					// 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-					// 	var x = d3.scale.ordinal()
-					// 		.domain(dataset[0].map(function (d) {
-					// 			return d.x;
-					// 		}))
-					// 		.rangeRoundBands([10, width - 10], 0.2);
-
-					// 	var y = d3.scale.linear()
-					// 		.domain([0, d3.max(dataset, function (d) {
-					// 			return d3.max(d, function (d) {
-					// 				if(d.y === 1){
-					// 					return d.y * 6;
-					// 				}
-					// 				else{
-					// 					return d.y * 3;
-					// 				}
-					// 			});
-					// 		})])
-					// 		.range([height, 0]);
-
-					// 	var yAxis = d3.svg.axis()
-					// 		.scale(y)
-					// 		.orient("left")
-					// 		.ticks(5)
-					// 		.tickSize(-width, 0, 0)
-					// 		.tickFormat(function (d) {
-					// 			return d;
-					// 		});
-
-					// 	var xAxis = d3.svg.axis()
-					// 		.scale(x)
-
-					// 	svg.append("g")
-					// 		.attr("class", "y-axis")
-					// 		.call(yAxis)
-
-					// 	svg.append("g")
-					// 		.attr("class", "x-axis")
-					// 		.attr("transform", "translate(0," + height + ")")
-					// 		.call(xAxis);
-
-					// 	var groups = svg.selectAll("g.time")
-					// 		.data(dataset) 
-					// 		.enter().append("g")
-					// 		.attr("class", "time")
-					// 		.attr("fill", function (d, i) {
-					// 			return color[i];
-					// 		});
-
-					// 	var rect = groups.selectAll("rect")
-					// 		.data(function (d) { return d; })
-					// 		.enter().append("rect")
-					// 		.attr("x", function (d) {
-					// 			return x(d.x);
-					// 		})
-					// 		.attr("class", "bar-chart-bar")
-					// 		.attr("y", height)
-					// 		.attr("width", x.rangeBand())
-					// 		.attr("height", 0);
-
-					// 	rect.transition()
-					// 		.delay(function(d, i) { return i * 74; })
-					// 		.attr("y", function (d) {
-					// 			return y(d.y0 + d.y);
-					// 		})
-					// 		.attr("height", function (d) {
-					// 			return y(d.y0) - y(d.y0 + d.y);
-					// 		})
-
-					// 		rect.on("mouseover", function (d) {
-					// 			tooltip.style("display", null);
-					// 			tooltip.select("text").text(returnSource(d.label, d.y) ) ;
-					// 		})
-					// 		.on("mouseout", function () {
-					// 			tooltip.style("display", "none");
-					// 		})
-					// 		.on("mousemove", function (d) {
-					// 			var xPosition = d3.mouse(this)[0] - 15;
-					// 			var yPosition = d3.mouse(this)[1] - 25;
-					// 			tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
-					// 		});
-
-					// 	var tooltip = svg.append("g")
-					// 		.attr("class", "tooltip")
-					// 		.style("display", "none");
-
-					// 	tooltip.append("text")
-					// 		.attr("x", 15)
-					// 		.attr("dy", "1.2em")
-					// 		.style("text-anchor", "middle")
-					// 		.attr("font-size", "12px")
-					// 		.attr("font-weight", "bold");
-
-					// 	if(dataset[0].length > 14) {
-					// 		var parent = d3.select(".x-axis")[0][0];
-					// 		var ticks = d3.selectAll(".x-axis .tick")[0];
-					// 		for(var i = 1; i < ticks.length; i += 2){
-					// 			parent.removeChild(ticks[i]);
-					// 		}
-					// 	};
-
-					// 	function returnSource(label, articlesRead) {
-					// 		var tooltipText = label + " - " + articlesRead;
-					// 		return tooltipText;
-					// 	}
-					// }
 					
 					/* Creates array that is better suited for D3 parsing
 					 *  @articlesToParse -> List of articles that user has read, obtained from JSON file
@@ -643,6 +518,8 @@ chrome.runtime.sendMessage({type: "getUser"}, function (user) {
 
 				var barchartDataset = createBarChartDataset(articlesReadThisWeek);
 				console.log(barchartDataset);
+
+				createBarChart(barchartDataset);
 
 				function getArticlesInTimespan(end, start) {
 					var articles = JSON.parse(JSON.stringify(userSnapshot.val().articles));
