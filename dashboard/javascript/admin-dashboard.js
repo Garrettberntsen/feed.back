f38529ba01e10c750312d04241d6a0ee3ecf9551
Leo Generali <leogen17@gmail.com>
2017-06-27 21:42:29 -0400

    Get start and end dates from form

--- dashboard/javascript/admin-dashboard.js
+++ dashboard/javascript/admin-dashboard.js
@@ -1,13 +1,35 @@
 function createAdminDashboardLink() {
 	var link = '<a class="sidebar__link" href=/dashboard/admin-dashboard.html><img class="sidebar__icon sidebar__icon--active" src="/app-assets/img/dashboard-icons/edit.svg"><p>Admin</p></a>';
-	console.log(link);
-
-  var sidebar = document.querySelector(".sidebar__items");
-  var div = document.createElement("div");
-  div.innerHTML = link;
-
-  sidebar.appendChild(div);
-
-  console.log(sidebar);
-
+	var sidebar = document.querySelector(".sidebar__items");
+	var div = document.createElement("div");
+	div.innerHTML = link;
+	sidebar.appendChild(div);
 }
+
+var model = {
+
+};
+
+var controller = {
+	init: function() {
+		model.form = document.querySelector(".date-picker"),
+		this.datepickerInit();
+	},
+
+	datepickerInit: function() {
+		var submitElem = document.querySelector(".date-picker-submit");
+		submitElem.addEventListener("click", controller.datepickerMod);
+	},
+
+	datepickerMod: function(e) {
+		e.preventDefault();
+		model.datestart = model.form[0].valueAsNumber; 
+		model.dateend = model.form[1].valueAsNumber; 
+	}
+};
+
+var views = {};
+
+
+//Kick off the process
+controller.init();
